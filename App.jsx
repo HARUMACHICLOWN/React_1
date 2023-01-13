4a24d373a1889905b09031eb01558fbb57d17710
HARUMACHICLOWN <111696192+HARUMACHICLOWN@users.noreply.github.com>
2023-01-13 10:42:11 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,166 @@
+import logo from './logo.svg';
+import './App.css';
+import { getByText, render } from '@testing-library/react';
+
+function App() {
+  // return <div>
+  //   текст123
+  //   <p>456</p>
+  //   <p>789</p>
+  // </div>
+  // return <div>
+  //   <input type="text" /><br />
+  //   <input type="text" />
+  // </div>
+  // return <div>
+  //   <ul>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   <li></li>
+  //   </ul>
+  // </div>
+// return <div>
+//   <table>
+//     <tr>
+//       <th>1</th>
+//       <th>2</th>
+//       <th>3</th>
+//     </tr>
+//     <tr>
+//       <th>3</th>
+//       <th>2</th>
+//       <th>1</th>
+//     </tr>
+//   </table>
+// </div>
+// return <div>
+//   <table>
+//     <th style={{color:'black'}}>1</th>
+//     <th style={{color:'green'}}>2</th>
+//     <th style={{color:'white'}}>3</th>
+//   </table>
+// </div>
+
+
+
+// return <div>текст</div>
+
+// const text = "text"
+// return <div>
+//   {text}
+// </div>
+
+// const text = <p>текст</p>
+// return <div>{text}</div>
+
+// const text1 = <p>текст1</p>
+// const text2 = <p>текст2</p>
+// return <div>
+//   {text1}
+//   {text2}
+// </div>
+
+
+// const attr = 'block'; 
+// return <div id={attr}>текст</div>
+//
+
+
+// const str = 'block'; 
+// return <div class={str}>текст</div>
+
+
+// const mystyle = { 
+//   color:'green',
+//   borderRadius: '30px',
+// };
+// return <div style={mystyle}>Text</div>
+
+
+
+
+// const show = true; 
+
+// if (show) { 
+//   var text = 'текст1';
+// } else { 
+//   var text = 'текст2';
+// }
+
+// return <div>{text}</div>
+
+// const arr = ['a', 'b', 'c', 'd', 'e'];
+
+// const list = arr.map(function(item, index) {
+//   return <li key={index}>{item}</li>;
+// });
+
+// return <ul>
+//   {list}
+// </ul>
+
+
+
+// class App extends React.Component {
+// 	getText() {
+// 		return <p>текст</p>;
+// 	}
+
+// 	render() {
+// 		const text = this.getText();
+// 		return <div>
+// 			{text}
+// 		</div>
+// 	}
+// }
+
+
+
+// class App extends React.Component {
+// 	getNum1() {
+// 		return 1;
+// 	}
+
+// 	getNum2() {
+// 		return 2;
+// 	}
+
+// 	render() {
+// 		return <div>
+// 			текст { this.getNum1() + this.getNum2() }
+// 		</div>
+// 	}
+// }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
